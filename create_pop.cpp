3dad447e0f91fc41edc19cdaee3057d8bb51a998
Brian Okinyi <brianokinyi.bo@gmail.com>
2018-06-08 00:02:58 -0700

    Linked list

---
+++ create_pop.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+class linked_list
+{
+private:
+    node *head,*tail;
+public:
+    linked_list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+
+void add_node(int n)
+    {
+        node *tmp = new node;
+        tmp->data = n;
+        tmp->next = NULL;
+
+        if(head == NULL)
+        {
+            head = tmp;
+            tail = tmp;
+        }
+        else
+        {
+            tail->next = tmp;
+            tail = tail->next;
+        }
+    }
+
+
+ node* gethead()
+    {
+        return head;
+    }
+
+
+    static void display(node *head)
+    {
+        if(head == NULL)
+        {
+            cout << "NULL" << endl;
+        }
+        else
+        {
+            cout << head->data << endl;
+            display(head->next);
+        }
+    }
+
+
+void front(int n)
+    {
+        node *tmp = new node;
+        tmp -> data = n;
+        tmp -> next = head;
+        head = tmp;
+    }
+
+    void after(node *a, int value)
+    {
+        node* p = new node;
+        p->data = value;
+        p->next = a->next;
+        a->next = p;
+    }
+};
+
+
+int main()
+{
+    linked_list a;
+    a.add_node(1);
+    a.add_node(2);
+    a.front(3);
+    a.front(4);
+    a.add_node(5);
+    linked_list::display(a.gethead());
+    return 0;
+}
